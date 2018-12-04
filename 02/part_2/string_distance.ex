4dc92654cf8752c31688e8304bd3cf8a7467c8bb
Jon Lunsford <jon@capturethecastle.net>
2018-12-03 21:32:20 -0800

    Part 2

---
+++ 02/part_2/string_distance.ex
@@ -0,0 +1,56 @@
+defmodule StringDistance do
+  def run(file) when is_binary(file) do
+    file
+    |> File.stream!([], :line)
+    |> Enum.to_list()
+    |> closest()
+  end
+
+  def closest(input) when is_list(input) do
+    input
+    |> Enum.map(&String.to_charlist/1)
+    |> find_charlist()
+  end
+
+  def find_charlist([head | tail]) do
+    if closest = Enum.find(tail, &closest_charlist(&1, head)) do
+      head
+      |> Enum.zip(closest)
+      |> Enum.filter(fn {left, right} -> left == right end)
+      |> Enum.map(fn {left, _} -> left end)
+      |> List.to_string()
+    else
+      find_charlist(tail)
+    end
+  end
+
+  defp closest_charlist(left, right) do
+    left
+    |> Enum.zip(right)
+    |> Enum.count(fn {left_point, right_point} ->
+      left_point != right_point
+    end)
+    |> case do
+      1 -> true
+      _ -> false
+    end
+  end
+end
+
+ExUnit.start()
+
+defmodule StringDistanceTest do
+  use ExUnit.Case
+
+  test "it returns strings that are different by only on char" do
+    assert StringDistance.closest([
+             "abcde",
+             "fghij",
+             "klmno",
+             "pqrst",
+             "fguij",
+             "axcye",
+             "wvxyz"
+           ]) == "fgij"
+  end
+end
